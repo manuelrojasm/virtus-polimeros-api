1f40f6d12da91ab448cb5d17905b1b330c1c7e5c
sgamezca <santilink1998@hotmail.com>
2025-03-23 12:53:25 -0500

    base API

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('contacto', 'ContactoController::index');
-$routes->post('contacto', 'ContactoController::create');
